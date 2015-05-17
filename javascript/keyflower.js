bea957f86413e6a8e6619aa465e3dc3d9c98e16c
Matthew Johnson <matthew@mcjohnsons.com>
2015-05-17 15:48:41 -0600

    Added a whole bunch of rendering stuff

--- javascript/keyflower.js
+++ javascript/keyflower.js
@@ -65,28 +65,91 @@ window.keyflower = {
 
             // Grabbing rules for current player count
             var rules = _.findWhere(keyflower.data.rules.playerCounts, {number: playerCount});
+            var tempPlayerCount = playerCount;
             var thisGame = keyflower.currentGame;
 
             // Split out by season
             thisGame.tilePool.home = _.where(tileCollection, {type: "home"});
-            //thisGame.tilePool.order = _.where(tileCollection, {type: "order"});
             //thisGame.tilePool.boat = _.where(tileCollection, {type: "boat"});
             thisGame.tilePool.spring = _.where(tileCollection, {type: "spring"});
             thisGame.tilePool.summer = _.where(tileCollection, {type: "summer"});
             thisGame.tilePool.autumn = _.where(tileCollection, {type: "autumn"});
 
+            // Shuffle the stacks
+            thisGame.tilePool.home = _.shuffle(thisGame.tilePool.home);
+            thisGame.tilePool.spring = _.shuffle(thisGame.tilePool.spring);
+            thisGame.tilePool.summer = _.shuffle(thisGame.tilePool.summer);
+            thisGame.tilePool.autumn = _.shuffle(thisGame.tilePool.autumn);
+
             // Sample the appropriate number of tiles from the stacks
             // This should reduce the size
             thisGame.tiles.home = _.sample(thisGame.tilePool.home, rules.homeCount);
+            // Assign each home tile to a player
+            _.each(thisGame.tiles.home, function (el) {
+                el.player = "Player " + tempPlayerCount;
+                if (tempPlayerCount > 1) {
+                    tempPlayerCount = tempPlayerCount-1;
+                }
+            }, this);
+
             thisGame.tiles.spring = _.sample(thisGame.tilePool.spring, rules.springCount);
             thisGame.tiles.summer = _.sample(thisGame.tilePool.summer, rules.summerCount);
             thisGame.tiles.autumn = _.sample(thisGame.tilePool.autumn, rules.autumnCount);
+
+            //Let's render the results!
+
+            // Render General Setup Instructions
+            keyflower.renderInfo("component-info", "<strong>Component Info: </strong>" + rules.componentInstructions);
+            keyflower.renderInfo("order-info", "<strong>Turn Order Tiles: </strong>" + rules.orderTileInstructions);
+            keyflower.renderInfo("winter-info", "<strong>Winter Tiles: </strong>Randomly select and distribute " + rules.winterCount + " tiles per player.");
+
+            // Start with home tiles...
+            keyflower.renderInfo("home-info", "Use the following home tiles for this game:");
+            keyflower.renderList("home-list", _.sortBy(thisGame.tiles.home, "name"), true);
+            // Then spring tiles...
+            keyflower.renderInfo("spring-info", "Use the following spring tiles for this game:");
+            keyflower.renderList("spring-list", _.sortBy(thisGame.tiles.spring, "name"), false);
+            // Then summer tiles...
+            keyflower.renderInfo("summer-info", "Use the following summer tiles for this game:");
+            keyflower.renderList("summer-list", _.sortBy(thisGame.tiles.summer, "name"), false);
+            // Then autumn tiles...
+            keyflower.renderInfo("autumn-info", "Use the following autumn tiles for this game:");
+            keyflower.renderList("autumn-list", _.sortBy(thisGame.tiles.autumn, "name"), false);
+
+            // Hide settings and show results
+            $("#setupPanel").collapse('show');
+            $("#settingsPanel").collapse('hide');
         }
     },
-    loadSettings: function() {
+    loadSettingsAndRandomize: function () {
+        var expansions = [];
+        var playerCount = 2;
+        // Load settings
+        var baseGame = $("#includeBaseKeyflower");
+        var farmersGame = $("#includeKeyflowerFarmers");
+        var merchantsGame = $("#includeKeyflowerMerchants");
+        var promoTiles = $("#includeKeyflowerPromos");
+        var playerCountElement = $("#playerCount");
 
+        if (baseGame && baseGame.checked) {
+            expansions.push("base");
+        }
+        if (farmersGame && farmersGame.checked) {
+            expansions.push("farmers");
+        }
+        if (merchantsGame && merchantsGame.checked) {
+            expansions.push("merchants");
+        }
+        if (promoTiles && promoTiles.checked) {
+            expansions.push("promos");
+        }
+        if (playerCountElement && playerCountElement.value) {
+            playerCount = playerCountElement.value;
+        }
+
+        keyflower.randomize(playerCount, expansions);
     },
-    verifyData: function() {
+    verifyData: function () {
         console.log("In verifyData().");
         if (keyflower.data && keyflower.data.games) {
             if (_.isArray(keyflower.data.games) && keyflower.data.games.length == 4) {
@@ -94,5 +157,36 @@ window.keyflower = {
             }
         }
         return false;
+    },
+    renderList: function (id, array, showPlayer) {
+        var htmlElement = document.getElementById(id);
+        if (htmlElement)
+        {
+            var renderedHtml = "";
+            _.each(array, function (el, index) {
+                var listItem = "";
+                if (!showPlayer)
+                {
+                    listItem = _.template('<li class="list-group-item"><%= name %></li>');
+                }
+                else
+                {
+
+                    listItem = _.template('<li class="list-group-item"><span class="badge"><%= player %></span><%= name %></li>');
+                }
+                // Append rendered HTML
+                renderedHtml = renderedHtml + listItem(el);
+            }, this);
+            htmlElement.innerHTML = renderedHtml;
+        }
+    },
+    renderInfo: function(id, text) {
+        var htmlElement = document.getElementById(id);
+        if (htmlElement)
+        {
+            var templateItem = _.template('<em><%= infoText %></em>');
+            htmlElement.innerHTML = templateItem({infoText: text});
+
+        }
     }
 };
