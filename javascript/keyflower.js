643926e6c81f4677b21d155525a3d7bd873c005e
Matthew Johnson <matthew@mcjohnsons.com>
2015-06-07 15:18:30 -0600

    Updating functionality and layout, also fixing some bugs.

--- javascript/keyflower.js
+++ javascript/keyflower.js
@@ -53,15 +53,13 @@ window.keyflower = {
     randomize: function (playerCount, includeList) {
         if (keyflower.verifyData()) {
             var tileCollection = [];
+            // Combine tiles from all games in includeList
             _.each(keyflower.data.games, function(el, index) {
                 if (_.indexOf(includeList, el.id) >= 0)
                 {
                     tileCollection = _.union(tileCollection, el.tiles);
                 }
             }, this);
-            console.log(tileCollection);
-            // Tiles from all games combined
-            // TODO: Add feature to select which games you want to play with
 
             // Grabbing rules for current player count
             var rules = _.findWhere(keyflower.data.rules.playerCounts, {number: playerCount});
@@ -70,12 +68,12 @@ window.keyflower = {
 
             // Split out by season
             thisGame.tilePool.home = _.where(tileCollection, {type: "home"});
-            //thisGame.tilePool.boat = _.where(tileCollection, {type: "boat"});
+            thisGame.tilePool.boat = _.where(tileCollection, {type: "boat"});
             thisGame.tilePool.spring = _.where(tileCollection, {type: "spring"});
             thisGame.tilePool.summer = _.where(tileCollection, {type: "summer"});
             thisGame.tilePool.autumn = _.where(tileCollection, {type: "autumn"});
 
-            // Shuffle the stacks
+            // Shuffle the stacks.
             thisGame.tilePool.home = _.shuffle(thisGame.tilePool.home);
             thisGame.tilePool.spring = _.shuffle(thisGame.tilePool.spring);
             thisGame.tilePool.summer = _.shuffle(thisGame.tilePool.summer);
@@ -84,6 +82,27 @@ window.keyflower = {
             // Sample the appropriate number of tiles from the stacks
             // This should reduce the size
             thisGame.tiles.home = _.sample(thisGame.tilePool.home, rules.number);
+            thisGame.tiles.spring = _.sample(thisGame.tilePool.spring, rules.springCount);
+            thisGame.tiles.summer = _.sample(thisGame.tilePool.summer, rules.summerCount);
+            thisGame.tiles.autumn = _.sample(thisGame.tilePool.autumn, rules.autumnCount);
+
+            // Get boats for player count
+            thisGame.tiles.boat = [];
+
+            _.each(rules.boats, function (el) {
+                var chosenBoat = _.findWhere(thisGame.tilePool.boat, {id: el});
+                if (chosenBoat) {
+                    thisGame.tiles.boat.push(chosenBoat);
+                }
+            }, this);
+
+            // Clone sampled stacks before any modification
+            thisGame.tiles.boat = JSON.parse(JSON.stringify(thisGame.tiles.boat));
+            thisGame.tiles.home = JSON.parse(JSON.stringify(thisGame.tiles.home));
+            thisGame.tiles.spring = JSON.parse(JSON.stringify(thisGame.tiles.spring));
+            thisGame.tiles.summer = JSON.parse(JSON.stringify(thisGame.tiles.summer));
+            thisGame.tiles.autumn = JSON.parse(JSON.stringify(thisGame.tiles.autumn));
+
             // Assign each home tile to a player
             _.each(thisGame.tiles.home, function (el) {
                 el.player = "Player " + tempPlayerCount;
@@ -92,20 +111,34 @@ window.keyflower = {
                 }
             }, this);
 
-            thisGame.tiles.spring = _.sample(thisGame.tilePool.spring, rules.springCount);
-            thisGame.tiles.summer = _.sample(thisGame.tilePool.summer, rules.summerCount);
-            thisGame.tiles.autumn = _.sample(thisGame.tilePool.autumn, rules.autumnCount);
+
+            //Choose a side if boat tiles chosen
+            _.each(thisGame.tiles.summer, function (el) {
+                if (el.options) {
+                    // shuffle first
+                    el.options = _.shuffle(el.options);
+                    // then choose a side
+                    var side = _.sample(el.options, 1);
+                    // update the chosen tile
+                    el.name = el.name + side;
+                    el.options = null;
+                }
+            }, this);
+
 
             //Let's render the results!
 
             // Render General Setup Instructions
             keyflower.renderInfo("component-info", "<strong>Component Info: </strong>" + rules.componentInstructions);
             keyflower.renderInfo("order-info", "<strong>Turn Order Tiles: </strong>" + rules.orderTileInstructions);
-            keyflower.renderInfo("winter-info", "<strong>Winter Tiles: </strong>Randomly select and distribute " + rules.winterCount + " tiles per player.");
+            keyflower.renderInfo("winter-info", "<strong>Winter Tiles: </strong>Shuffle all winter tiles face down, select " + (rules.winterCount * rules.number) + " tiles, and deal " + rules.winterCount + " to each player.");
 
             // Start with home tiles...
             keyflower.renderInfo("home-info", "Use the following home tiles for this game:");
             keyflower.renderList("home-list", _.sortBy(thisGame.tiles.home, "name"), true);
+            // Then boat tiles...
+            keyflower.renderInfo("boat-info", "Use the following boat tiles for this game:");
+            keyflower.renderList("boat-list", _.sortBy(thisGame.tiles.boat, "id"), false);
             // Then spring tiles...
             keyflower.renderInfo("spring-info", "Use the following spring tiles for this game:");
             keyflower.renderList("spring-list", _.sortBy(thisGame.tiles.spring, "name"), false);
@@ -125,23 +158,28 @@ window.keyflower = {
         var expansions = [];
         var playerCount = 2;
         // Load settings
-        var includeGames = $("#criteriaIncludeGames");
+        var includeBase = $("#criteriaIncludeBase");
+        var includeFarmers = $("#criteriaIncludeFarmers");
+        var includeMerchants = $("#criteriaIncludeMerchants");
         var playerCountElement = $("#criteriaPlayerCount");
 
-        if (includeGames && includeGames.val()) {
-            expansions = includeGames.val();
+        if (includeBase && includeBase.is(':checked')) {
+            expansions.push("base");
+        }
+        if (includeFarmers && includeFarmers.is(':checked')) {
+            expansions.push("farmers");
+        }
+        if (includeMerchants && includeMerchants.is(':checked')) {
+            expansions.push("merchants");
         }
 
         if (playerCountElement && playerCountElement.val()) {
             playerCount = parseInt(playerCountElement.val());
         }
 
-        console.log(expansions);
-
         keyflower.randomize(playerCount, expansions);
     },
     verifyData: function () {
-        console.log("In verifyData().");
         if (keyflower.data && keyflower.data.games) {
             if (_.isArray(keyflower.data.games) && keyflower.data.games.length == 4) {
                 return true;
